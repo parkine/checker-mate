45800bb12c15209e19e64cec36d5d026bd55ef8c
ine <inepark22@gmail.com>
2025-10-15 11:05:56 -0400

    implemented the function to move the pieces, jump, make the piece king

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 # main.py
 import pygame
-from board import Board
 from constants import *
+from game import Game
 
-# Initialize pygame
-pygame.init()
+FPS = 60
 
-# Create the window (same size as your board)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Checkers")
+pygame.display.set_caption('Checkers')
 
-board = Board()
+def get_row_col_from_mouse(pos):
+  x, y = pos
+  row = y // SQUARE_WIDTH
+  col = x // SQUARE_WIDTH
+  return row, col
 
-# Main loop
-run = True
-while run:
+def main():
+  run = True
+  clock = pygame.time.Clock()
+  game = Game(WIN)
+
+  while run:
+    clock.tick(FPS)
+
+    # if game.winner() != None:
+    #     print(game.winner())
+    #     run = False
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+      if event.type == pygame.QUIT:
+        run = False
 
-    board.draw(WIN)
-    pygame.display.update()
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        pos = pygame.mouse.get_pos()
+        row, col = get_row_col_from_mouse(pos)
+        game.select(row, col)
 
-pygame.quit()
+    game.update()
 
+  pygame.quit()
+
+main()
